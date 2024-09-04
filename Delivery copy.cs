e5df35f5ca21920a524603880f61b8e02b2e7e99
achinoam <achinoamasulin123@gmail.com>
2024-09-04 22:49:53 +0300

    first commit

---
+++ Delivery copy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] float destroyDelay = 0.4f;
+    bool hasPackage = false;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Boom!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package Picked Up!");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+        }
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package Deliverd!");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+}
+
